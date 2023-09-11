9d1b85af0686f859cfb24e2b41fca358eec722cf
Yijia Chen <kevinc110604@gmail.com>
2023-09-11 15:07:33 -0700

    [new] Add API of list_waiting_queues &
    read_waiting_queue_items & CORS header

--- app.py
+++ app.py
@@ -11,5 +11,23 @@ def swap_waiting_q_items():
     output = {"success": resp}
     return jsonify(output)
 
+@app.route("/list_waiting_queues", methods=["GET"])
+def list_waiting_queues():
+    output = {"waiting_queues": qm.list_waiting_queues()}
+    return jsonify(output)
+
+@app.route("/read_waiting_queue_items", methods=["GET"])
+def read_waiting_queue_items():
+    args = request.args.to_dict()
+    output = {"waiting_queue_items": qm.read_waiting_queue_items(**args)}
+    return jsonify(output)
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 if __name__ == "__main__":
     app.run(debug=True, port=7088)
